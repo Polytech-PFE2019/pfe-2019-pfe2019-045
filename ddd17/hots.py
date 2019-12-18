a19b5343b81acc56ef09fc7b071d5310996441ba
YileiLI <lily_19950906@126.com>
2019-12-18 21:51:07 +0100

    MOD update HOTS
    model but feed with first 50ms data

--- ddd17/hots.py
+++ ddd17/hots.py
@@ -1,10 +1,11 @@
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 np.set_printoptions(threshold=np.inf)
 
-filename ='/Volumes/SD/exported_9175.h5'
+filename = '/Volumes/SD/exported_9175.h5'
 
 
 def traverse_datasets(hdf_file):
@@ -30,7 +31,7 @@ def ed(a, b):
     :param b: vector b
     :return: euclidian distance between a & b
     """
-    #np.sqrt(np.sum(np.square(vector1-vector2)))
+    # np.sqrt(np.sum(np.square(vector1-vector2)))
     return np.linalg.norm(a - b)
 
 
@@ -41,10 +42,87 @@ def time_context(data, event, r):
     p = event[3]
     tc = np.zeros((data.shape[0], 4), dtype=np.int16)
 
-    for i in range(x-r, x+r):
-        for j in range(y-r, y+r):
-            print(np.where(data[:,]))
+    for i in range(x - r, x + r):
+        for j in range(y - r, y + r):
+            print(np.where(data[:, ]))
 
+
+def model(data, n, k, r, t_c):
+    """
+    Hierarchical Model from HOTS
+    @param data: events or the output from previous layer
+    @param n: polarity
+    @param k: k cluster
+    @param r: radius of neighborhood
+    @param t_c: time constant
+    @return: clustered events
+    """
+    R = 2 * r + 1
+
+    temp = np.zeros((260 + 2 * r, 346 + 2 * r), dtype=np.int32)
+
+
+    feat_c = np.zeros((data.shape), dtype=np.int32)
+    size_pre = 0
+
+    for m in range(n):
+        data_on = data[np.where(data[:, 3] == m)]
+        feat = np.zeros((data_on.shape), dtype=np.int32)
+        C = np.zeros((k, R ** 2), dtype=np.double)
+        p = np.ones(k)
+        print(m)
+        print(data_on.shape)
+        print(feat.shape)
+        for j in range(k):
+            rand = random.randint(0, data_on.shape[0])
+            temp[data_on[rand][2] + r][data_on[rand][1] + r] = data_on[j][0]
+            t = np.array(
+                temp[(data_on[j][2]): (data_on[j][2] + 2 * r + 1),
+                (data_on[j][1]): (data_on[j][1] + 2 * r + 1)]).flatten()
+            C[j] = np.exp((t - data_on[j][0]) / t_c)
+            feat[j] = data_on[j]
+            feat[j][3] = k * m + j
+
+        for i in range(data_on.shape[0]):
+            temp[data_on[i][2] + r][data_on[i][1] + r] = data_on[i][0]
+            t = np.array(
+                temp[(data_on[i][2]): (data_on[i][2] + 2 * r + 1),
+                (data_on[i][1]): (data_on[i][1] + 2 * r + 1)]).flatten()
+            S = np.exp((t - data_on[j][0]) / t_c)
+            dist_min = ed(S, C[0])
+            index = 0
+            for j in range(1, k):
+                dist = ed(S, C[j])
+                if dist < dist_min:
+                    dist_min = dist
+                    index = j
+
+            feat[i] = data_on[i]
+            feat[i][3] = k * m + index
+
+            alpha = 0.01 / (1 + p[index] / 20000)
+            beta = np.cos(C[j].reshape(R, R), S.reshape(R, R))
+            C[index] = C[index] + alpha * (S - beta.flatten() * C[index])
+
+            p[index] += 1
+        feat_c[size_pre:(size_pre + data_on.shape[0])] = feat.copy()
+        size_pre = data_on.shape[0]
+
+    fig, axes = plt.subplots(n, k, figsize=(15, 10))
+
+    for j in range(k * n):
+        temp[data_on[j][2] + r][data_on[j][1] + r] = data_on[j][0]
+        frame = np.zeros((260, 346), dtype=np.int16)
+        cluster = feat_c[np.where(feat_c[:, 3] == j)]
+        for i in range(cluster.shape[0]):
+            frame[cluster[i][2]][cluster[i][1]] = 1
+        axes.ravel()[j].imshow(frame, cmap='gray')
+        axes.ravel()[j].set_title(str(j))
+        print("cluster ", j)
+        print(cluster.shape[0])
+    fig.tight_layout()
+    plt.show()
+    return feat_c
 
 
 if __name__ == '__main__':
@@ -55,186 +133,21 @@ if __name__ == '__main__':
     events = np.array(data, dtype=np.int32)
     d.close()
 
-    p = 1
-    k = 5
-    r = 2
-    R = 2 * r + 1
+    events = events[np.where(events[:, 0] - events[0, 0] <= 50000)]
+    print(events.shape)
+
+    k = 2
+    r = 5
     t_c = 50
 
-    temp = np.zeros((260+2*r, 346+2*r), dtype=np.int32)
-    C = np.zeros((k, R**2), dtype=np.double)
-    p = np.ones(k)
+    out = model(events, 2, k, r, t_c).copy()
 
-    i = k
-    data_on = events[np.where(events[:, 3] == 1)]
-    feat = np.zeros((data_on.shape), dtype=np.int32)
-
-    for j in range(k):
-
-        temp[data_on[j][2]+r][data_on[j][1]+r] = data_on[j][0]
-        t = np.array(temp[(data_on[j][2]): (data_on[j][2]+2*r+1), (data_on[j][1]): (data_on[j][1]+2*r+1)]).flatten()
-        C[j] = np.exp((t-data_on[j][0])/t_c)
-        feat[j] = data_on[j]
-        feat[j][3] = j
-
-    while data_on[i][0] - data_on[0][0] <= 50000:
-        temp[data_on[i][2] + r][data_on[i][1] + r] = data_on[i][0]
-        t = np.array(
-            temp[(data_on[i][2]): (data_on[i][2] + 2 * r + 1), (data_on[i][1]): (data_on[i][1] + 2 * r + 1)]).flatten()
-        S = np.exp((t - data_on[j][0]) / t_c)
-        dist_min = ed(S, C[0])
-        index = 0
-        for j in range(1, k):
-            dist = ed(S, C[j])
-            if dist < dist_min:
-                dist_min = dist
-                index = j
-
-        feat[i] = data_on[i]
-        feat[i][3] = index
-
-        alpha = 0.01/(1+p[index]/20000)
-        beta = np.cos(C[j].reshape(R, R), S.reshape(R, R))
-        C[index] = C[index]+alpha*(S-beta.flatten()*C[index])
-
-        p[index] += 1
-        i += 1
-
-    print(i)
-
-    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-
-    for j in range(k):
-        temp[data_on[j][2] + r][data_on[j][1] + r] = data_on[j][0]
-        frame = np.zeros((260, 346), dtype=np.int16)
-        cluster = feat[np.where(feat[:, 3] == j)]
-        for i in range(cluster.shape[0]):
-            frame[cluster[i][2]][cluster[i][1]] = 1
-        axes.ravel()[j].imshow(frame, cmap='gray')
-        axes.ravel()[j].set_title(str(j))
-    fig.tight_layout()
-    plt.show()
-
-    k = 8
-    i = k
-    r = 4
-    R = 2 * r + 1
-    t_c = 500
-    data_on = feat
-    feat = np.zeros((data_on.shape), dtype=np.int32)
-    temp = np.zeros((260 + 2 * r, 346 + 2 * r), dtype=np.int32)
-    C = np.zeros((k, R ** 2), dtype=np.double)
-    p = np.ones(k)
-
-    for j in range(k):
-        temp[data_on[j][2] + r][data_on[j][1] + r] = data_on[j][0]
-        t = np.array(
-            temp[(data_on[j][2]): (data_on[j][2] + 2 * r + 1), (data_on[j][1]): (data_on[j][1] + 2 * r + 1)]).flatten()
-        C[j] = np.exp((t - data_on[j][0]) / t_c)
-        feat[j] = data_on[j]
-        feat[j][3] = j
-
-    print(i)
-    while data_on[i][0] - data_on[0][0] <= 50000:
-        temp[data_on[i][2] + r][data_on[i][1] + r] = data_on[i][0]
-        t = np.array(
-            temp[(data_on[i][2]): (data_on[i][2] + 2 * r + 1), (data_on[i][1]): (data_on[i][1] + 2 * r + 1)]).flatten()
-        S = np.exp((t - data_on[j][0]) / t_c)
-        dist_min = ed(S, C[0])
-        index = 0
-        for j in range(1, k):
-            dist = ed(S, C[j])
-            if dist < dist_min:
-                dist_min = dist
-                index = j
-
-        feat[i] = data_on[i]
-        feat[i][3] = index
-
-        alpha = 0.01 / (1 + p[index] / 20000)
-        beta = np.cos(C[j].reshape(R, R), S.reshape(R, R))
-        C[index] = C[index] + alpha * (S - beta.flatten() * C[index])
-
-        p[index] += 1
-        i += 1
-
-    fig, axes = plt.subplots(2, 4, figsize=(15, 10))
-
-    for j in range(k):
-        temp[data_on[j][2] + r][data_on[j][1] + r] = data_on[j][0]
-        frame = np.zeros((260, 346), dtype=np.int16)
-        cluster = feat[np.where(feat[:, 3] == j)]
-        for i in range(cluster.shape[0]):
-            frame[cluster[i][2]][cluster[i][1]] = 1
-        axes.ravel()[j].imshow(frame, cmap='gray')
-        axes.ravel()[j].set_title(str(j))
-    fig.tight_layout()
-    plt.show()
-
-    k = 16
-    i = k
     r = 8
-    R = 2 * r + 1
+    t_c = 500
+
+    out = model(out, 4, k, r, t_c).copy()
+
+    r = 80
     t_c = 5000
-    data_on = feat
-    feat = np.zeros((data_on.shape), dtype=np.int32)
-    temp = np.zeros((260 + 2 * r, 346 + 2 * r), dtype=np.int32)
-    C = np.zeros((k, R ** 2), dtype=np.double)
-    p = np.ones(k)
 
-    for j in range(k):
-        t = np.array(
-            temp[(data_on[j][2]): (data_on[j][2] + 2 * r + 1), (data_on[j][1]): (data_on[j][1] + 2 * r + 1)]).flatten()
-        C[j] = np.exp((t - data_on[j][0]) / t_c)
-        feat[j] = data_on[j]
-        feat[j][3] = j
-
-    while data_on[i][0] - data_on[0][0] <= 50000:
-        temp[data_on[i][2] + r][data_on[i][1] + r] = data_on[i][0]
-        t = np.array(
-            temp[(data_on[i][2]): (data_on[i][2] + 2 * r + 1), (data_on[i][1]): (data_on[i][1] + 2 * r + 1)]).flatten()
-        S = np.exp((t - data_on[j][0]) / t_c)
-        dist_min = ed(S, C[0])
-        index = 0
-        for j in range(1, k):
-            dist = ed(S, C[j])
-            if dist < dist_min:
-                dist_min = dist
-                index = j
-
-        feat[i] = data_on[i]
-        feat[i][3] = index
-
-        alpha = 0.01 / (1 + p[index] / 20000)
-        beta = np.cos(C[j].reshape(R, R), S.reshape(R, R))
-        C[index] = C[index] + alpha * (S - beta.flatten() * C[index])
-
-        p[index] += 1
-        i += 1
-
-    fig, axes = plt.subplots(2, 8, figsize=(15, 10))
-
-    for j in range(k):
-        frame = np.zeros((260, 346), dtype=np.int16)
-        cluster = feat[np.where(feat[:, 3] == j)]
-        for i in range(cluster.shape[0]):
-            frame[cluster[i][2]][cluster[i][1]] = 1
-        axes.ravel()[j].imshow(frame, cmap='gray')
-        axes.ravel()[j].set_title(str(j))
-    fig.tight_layout()
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    out = model(out, 8, k, r, t_c).copy()
